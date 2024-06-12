4f09a6a4d4e747bfd1d071d332dd8950d00b6817
Pulkit Ujjainwal <pulkitujjainwal66@gmail.com>
2024-06-13 02:45:24 +0530

    Express-Validator

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,43 @@
+// authRoutes.js
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const { register, login } = require("../controllers/authController");
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post(
+  "/register",
+  [
+    check("email").isEmail().withMessage("Enter a valid email address"),
+    check("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+  register
+);
+
+router.post(
+  "/login",
+  [
+    check("email").isEmail().withMessage("Enter a valid email address"),
+    check("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+  login
+);
 
 module.exports = router;
