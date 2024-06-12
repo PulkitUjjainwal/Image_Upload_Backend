b412dce516fa0fedbb6b60db99a4abc71d77f6d3
Pulkit Ujjainwal <pulkitujjainwal66@gmail.com>
2024-06-13 02:40:11 +0530

    Rate limiting Enabled

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ const helmet = require("helmet");
 const { publishScheduledImages } = require("./src/util/cronJobs");
 const csrf = require("csurf");
 const csrfProtection = csrf({ cookie: true });
+const rateLimit = require("express-rate-limit");
 
 dotenv.config();
 
 const app = express();
 const PORT = 3003;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests from this IP, please try again later",
+});
 
 app.use(cors());
 app.use(helmet());
